181ce812ec90ab36abfb7deea66a1444da116359
mckellar11 <128339167+mckellar11@users.noreply.github.com>
2025-02-18 19:50:10 -0500

    Update balatro Setup.bat
    tells people what my script does even tho ChatGPT put some info in there
    
    Signed-off-by: mckellar11 <128339167+mckellar11@users.noreply.github.com>

--- balatro Setup.bat
+++ balatro Setup.bat
@@ -1,7 +1,7 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal enabledelayedexpansion                                ----With delayed expansion enabled, variables can be evaluated at runtime, which means you can use variables inside loops or if-else blocks and their values will be updated as the script runs.
 
-:: Get the current user's name and define paths
+:: Get the current user's name and define paths                  ---description says it but it defines you, balatro install location, mod dir, and then the soon to be created folders and files
 set USERPROFILE=%USERPROFILE%
 set BALATRO_DIR=%USERPROFILE%\AppData\Roaming\Balatro
 set MODS_DIR=%BALATRO_DIR%\Mods
@@ -9,23 +9,23 @@ set NATIVEFS_DIR=%BALATRO_DIR%\nativefs
 set SMODS_DIR=%BALATRO_DIR%\SMODS
 set LOVELY_FILE=%BALATRO_DIR%\lovely.lua
 
-:: Create necessary folders
+:: Create necessary folders                    < ----they say it here 
 mkdir "%NATIVEFS_DIR%" 2>nul
 mkdir "%SMODS_DIR%" 2>nul
 
-:: Create lovely.lua file
+:: Create lovely.lua file                        <----and here. tells lovely file what to have in it
 echo return { > "%LOVELY_FILE%"
 echo   repo = "https://github.com/ethangreen-dev/lovely-injector", >> "%LOVELY_FILE%"
 echo   version = "0.7.1", >> "%LOVELY_FILE%"
 echo   mod_dir = "/data/data/com.unofficial.balatro/files/save/game/Mods", >> "%LOVELY_FILE%"
 echo } >> "%LOVELY_FILE%"
 
-:: Check for Flower Pot mod
+:: Check for Flower Pot mod                            <----- just keep reading here
 for /d %%D in ("%MODS_DIR%\*") do (
     echo %%~nxD | findstr /i "flower.pot" >nul && set FP_DIR=%%D
 )
 
-if defined FP_DIR (
+if defined FP_DIR (                                                        ----- if its here take the files and rename them, and moves them 
     set FP_LIBS=!FP_DIR!\libs
     if exist "!FP_LIBS!\json.lua" (
         copy /Y "!FP_LIBS!\json.lua" "%BALATRO_DIR%\FP_json.lua"
@@ -35,7 +35,7 @@ if defined FP_DIR (
     )
 )
 
-:: Copy files from other mods
+:: Copy files from other mods                                                                                ----should be the last "required files" and lovely dumps
 copy /Y "%USERPROFILE%\AppData\Roaming\Balatro\Mods\Talisman\nativefs.lua" "%NATIVEFS_DIR%" >nul 2>&1
 copy /Y "%MODS_DIR%\smods-main\version.lua" "%SMODS_DIR%" >nul 2>&1
 xcopy /E /Y "%MODS_DIR%\lovely\dump\*" "%BALATRO_DIR%" >nul 2>&1
